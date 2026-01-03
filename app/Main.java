555519e3c32204384d17efca8dfb6a290e528a52
frolov-maks <ffrolov.maks@gmail.com>
2026-01-03 16:14:01 +0200

    text changing

--- app/Main.java
+++ app/Main.java
@@ -2,6 +2,6 @@ package app;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("This is shop-online app");
+        System.out.printf("Shop-online for customers");
     }
 }
